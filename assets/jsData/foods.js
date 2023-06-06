511b0f2bcfbc5495d2012526e604b23cf3ec1754
Anmol Burmy <anmolburmy145@gmail.com>
2023-06-06 15:05:26 -0500

    fixed css for login and register page

--- assets/jsData/foods.js
+++ assets/jsData/foods.js
@@ -1,21 +1,21 @@
-export default foods = [
-  {
-    loc: require("../images/pancake.png"),
-    like: 0,
-    categories: "american (new)",
-  },
-  {
-    loc: require("../images/pancake.png"),
-    like: 0,
-    categories: "american (Traditional)",
-  },
-  { loc: require("../images/pancake.png"), like: 0, categories: "armenian" },
-  {
-    loc: require("../images/pancake.png"),
-    like: 0,
-    categories: "Asian Fusion",
-  },
-  { loc: require("../images/pancake.png"), like: 0, categories: "asturian" },
-  { loc: require("../images/pancake.png"), like: 0, categories: "chinese" },
-  { loc: require("../images/pancake.png"), like: 0, categories: "italian" },
-];
+// export default foods = [
+//   {
+//     loc: require("../images/pancake.png"),
+//     like: 0,
+//     categories: "american (new)",
+//   },
+//   {
+//     loc: require("../images/pancake.png"),
+//     like: 0,
+//     categories: "american (Traditional)",
+//   },
+//   { loc: require("../images/pancake.png"), like: 0, categories: "armenian" },
+//   {
+//     loc: require("../images/pancake.png"),
+//     like: 0,
+//     categories: "Asian Fusion",
+//   },
+//   { loc: require("../images/pancake.png"), like: 0, categories: "asturian" },
+//   { loc: require("../images/pancake.png"), like: 0, categories: "chinese" },
+//   { loc: require("../images/pancake.png"), like: 0, categories: "italian" },
+// ];
